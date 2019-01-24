69a7794c119bc228bcf27f48744dce3204737603
Tetraim <megahulk@gmail.com>
2019-01-24 07:13:05 -0800

    version1

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search-panel.css';
+
+const SearchPanel = () => {
+   
+    return (
+        <input type= "text"
+        className="search-panel search-input"
+        placeholder="type to search">
+        </input>
+    );
+};
+
+export default SearchPanel;
